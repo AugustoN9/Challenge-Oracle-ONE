cf7b0044a659cfd6b70756087fa90b87e5494807
AugustoN9 <auguston8@hotmail.com>
2022-02-03 12:02:00 -0300

    update do teclado.js

--- teclado.js
+++ teclado.js
@@ -25,9 +25,78 @@ if(keys && keyPad && display)
                 {
                     display.innerText += this.dataset.key.toUpperCase();
                 }else{
-                    display.innerText += this.dataset.key.toLowerCase();
+                    display.innerText += this.dataset.key.toUpperCase();
                 }
             }
         });
     });
 }
+
+
+    document.querySelectorAll("button").forEach(function(button){
+        button.addEventListener("click", function(event){
+
+            const el = event.target || event.srcElement;
+            const id = el.id;
+            var idSel = id;
+            console.log(idSel);
+            if(idSel != null){
+               
+                confereLetra(idSel);                
+            }
+        })
+    })
+
+function confereLetra(id){
+
+        var teclado = document.getElementById(id).value;
+
+        var palavra = 'ABACAXI';
+        var palavrasecreta = document.querySelector("#palavrasecreta");
+        palavrasecreta.innerText = palavra;
+
+        console.log('A palavra secreta eh '+palavrasecreta.innerText);
+    
+        var string = palavrasecreta.innerText;
+        var cont = 0;
+
+        for(var i = 0; i < string.length; i++){
+            if(teclado == string[i]){
+                cont = ++cont;
+            }            
+        }     
+        if(cont > 0 ){
+            esconde(id);
+            exibirLetraSecreta();
+            console.log('A letra '+teclado+' foi encontrada '+cont+' vezes');  
+        }else{
+            exibirLetraSecreta();
+        }
+         
+}
+
+function exibirLetraSecreta(){
+
+    var tecla_off = document.querySelector('.secreta');
+    var estado = tecla_off.style.display;
+
+    tecla_off.style.display = estado == 'none' ? 'inline':'none';
+}
+
+
+
+function esconde(tecla) {  
+
+    //var tecla_on = document.querySelector(tecla);
+    var tecla_on = document.getElementById(tecla);
+
+    var estado = tecla_on.style.display;
+
+    tecla_on.style.display = estado == 'inline' ? 'none':'inline';
+
+    /*
+    if(tecla_on == 'inline') {  
+        tecla_on = 'none';  
+     }    
+     */    
+ }
